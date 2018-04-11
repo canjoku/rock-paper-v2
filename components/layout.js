8f4bedca523a05bc15327a8a02f58c2100e42b40
Tony <ca.njoku@yahoo.com>
2018-04-11 17:38:55 +0100

    completed configuration for scss

--- components/layout.js
+++ components/layout.js
@@ -1,10 +1,11 @@
 import { Component } from 'react'
 import Footer from '../components/footer'
+import '../scss/application.scss'
 
 class Layout extends Component {
   render () {
     return <div>
-    <h1>WELCOME TO ROCK-PAPER-SCISSORS-GAME</h1>
+      <h1>WELCOME TO ROCK-PAPER-SCISSORS-GAME</h1>
       { this.props.children }
       <Footer />
     </div>
